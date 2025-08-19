0c8912ea62fafff31c75f025ea79552ac987463a
Ryuki-4648 <hato_poppo73@yahoo.co.jp>
2025-08-19 10:58:33 +0900

    update: store修正

--- quizrium/src/lib/store.ts
+++ quizrium/src/lib/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { quizSlice } from "./features/quiz/quizSlice"; // quizSliceをインポート
 
 export const store = configureStore({
   reducer: {
@@ -6,10 +7,15 @@ export const store = configureStore({
   // ここが空の場合「Store does not have a valid reducer. Make sure the argument passed to combineReducers is an object whose values are reducers.」のエラーが発生する
   // クライアントとサーバーで状態が一致せず、Hydration Error が起きることがある。以下のエラー文参照。
   // Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:
-},
+
+  // TODO: 作成したquizReducerをインポートし、storeのreducerに追加
+    quiz: quizSlice.reducer, // quizSliceのreducerを追加
+  },
+
   // 開発環境ではDevToolsを有効化
   devTools: process.env.NODE_ENV !== 'production',
-  });
-  // TypeScriptでReduxを使うための型定義
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppDispatch = typeof store.dispatch;
+});
+
+// TypeScriptでReduxを使うための型定義
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
